ef549e377c11edcb2c5a676bcde4c142b4219448
sharath0036 <ksharathbabu2@gmail.com>
2025-08-22 17:04:33 +0530

    laundry ui fixed

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
+import { router as routerForUtility } from "./Utility/routes.js";
+import { router as routerFromCommon } from "./Common/routes.js";
+import { router as routerFromCustom } from "./Custom/routes.js";
+import { router as routerFromLogin } from "./Login/routes.js";
+import { router as routerFromV1 } from "./V1/routes.js";
+
+import { router as Cors } from "./Cors/routes.js";
+
+// import { router as routerFromBinV5Secured } from "./binV5Secured/routes.js";
+import { router as routerFromBinV4 } from "./binV4/routes.js";
+
+import { StartFunc as StartFuncPortListen } from "./PortListen.js";
+
+import { StartFunc as MiddleWaresBinSecured } from "./MiddleWares/MiddleWares.binSecured/EntryFile.js";
+// import { StartFunc as StartFuncKWSServer } from "./Projects/KWSServer/EntryFile.js";
+
 import express from 'express';
+import http from 'http';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+global.__basedir = path.dirname(__filename);
+
 const app = express();
-// const port = 3000;
+const server = http.createServer(app);
 
-var port = normalizePort(process.env.PORT || 3000);
+var port = normalizePort(process.env.PORT || '7019');
 
-import { StartFunc as StartFuncFromMiddleware } from "./Token/MiddleWares/entryFile.js";
+// app.disable('x-powered-by');
 
-import { router as routerFromToken } from "./Token/routes.js";
-import { router as routerFromV1 } from "./V1/routes.js";
-import { router as routerFromSV1 } from "./SV1/routes.js";
-
-app.use(express.static('Public'));
 app.use(cookieParser());
 
-app.use("/Token", routerFromToken);
-app.use("/V1", routerFromV1);
-app.use("/SV1", StartFuncFromMiddleware, routerFromSV1);
+app.use(express.json({ limit: '100mb' }));
+
+app.use('/', express.static(path.join(path.resolve(), 'public')));
+
+app.get("/k1", (req, res) => {
+    res.end("this is k1");
+})
+
+const CommonCorsFunc = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+};
+
+app.use('/binV4', routerFromBinV4);
+app.use('/V1', routerFromV1);
+// app.use('/binV5Secured', MiddleWaresBinSecured, routerFromBinV5Secured);
+
+app.use('/utility', routerForUtility);
+app.use('/Common', routerFromCommon);
+app.use('/Custom', routerFromCustom);
+app.use('/Login', routerFromLogin);
+app.use('/Cors', CommonCorsFunc, Cors);
+
+// StartFuncKWSServer(server);
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
@@ -36,7 +76,4 @@ function normalizePort(val) {
     return false;
 };
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-    console.log(`Open here http://localhost:${port}`);
-});
+server.listen(port, StartFuncPortListen);
